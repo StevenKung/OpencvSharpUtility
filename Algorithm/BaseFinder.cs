e70619bbfb6fc3e3b4f5d5fbc1596ad8fe3cba70
StevenKung <s790408p@gmail.com>
2018-06-03 17:48:16 +0800

    add new files BaseFinder.cs abstract class MatTypeEditor.cs to show image in propertyGird

---
+++ Algorithm/BaseFinder.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing.Design;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenCvSharp;
+using OpencvSharpUtility.UI;
+
+namespace OpencvSharpUtility.Algorithm
+{
+    abstract class BaseFinder
+    {
+        protected Mat gray = new Mat();
+        protected Mat outputimg = new Mat();
+        
+        public Mat OutputImg { get { return outputimg; } }
+
+        public BaseFinder(Mat Src)
+        {
+            switch (Src.Channels())
+            {
+                case 1:
+                    gray = Src.Clone();
+                    Cv2.CvtColor(Src, outputimg, ColorConversionCodes.GRAY2BGR);
+                    break;
+
+                case 3:
+                    outputimg = Src.Clone();
+                    Cv2.CvtColor(Src, gray, ColorConversionCodes.BGR2GRAY);
+                    break;
+
+                default:
+                    throw new Exception("Source image must 1 or 3 channels");
+                    break;
+            }
+        }
+        public abstract object Find();
+
+
+    }
+}
---
+++ UI/MatTypeEditor.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing.Design;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.Design;
+using OpenCvSharp;
+
+namespace OpencvSharpUtility.UI
+{
+    class MatTypeEditor :UITypeEditor
+    {
+
+        public override UITypeEditorEditStyle GetEditStyle(ITypeDescriptorContext context)
+        {
+            return UITypeEditorEditStyle.Modal;
+        }
+        public override object EditValue(ITypeDescriptorContext context, System.IServiceProvider provider, object value)
+        {
+            IWindowsFormsEditorService svc = provider.GetService(typeof(IWindowsFormsEditorService)) as IWindowsFormsEditorService;
+            
+
+            UserControlPicBox control = new UserControlPicBox
+            {
+                SrcImg = value as Mat
+            };
+
+            Form form = new Form
+            {
+                AutoSize = true,
+
+            };
+            form.Controls.Add(control);
+            form.ShowDialog();
+            return value; // can also replace the wrapper object here
+        }
+
+
+    }
+}
